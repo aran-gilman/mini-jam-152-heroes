15cca3d6e35c727e3245c215692b4baada8a8eab
Aran Gilman <f.zoniventris@gmail.com>
2024-02-16 13:59:01 -0800

    First pass at sword animation

--- Assets/Scripts/PlayerAttack.cs
+++ Assets/Scripts/PlayerAttack.cs
@@ -30,7 +30,16 @@ public class PlayerAttack : MonoBehaviour
 
     private void HandleAttack(GameObject sword)
     {
-        Debug.Log($"Attacked at position {playerPointer.transform.position}");
+        Ray toPointer = new Ray(
+            transform.position, playerPointer.transform.position);
+
+        Vector3 swordPos = toPointer.GetPoint(swordDistanceFromPlayer);
+        Quaternion swordRotation = Quaternion.Euler(0, 0, 
+            Mathf.Rad2Deg * Mathf.Atan2(
+                -playerPointer.transform.position.x, playerPointer.transform.position.y));
+        sword.transform.SetPositionAndRotation(swordPos, swordRotation);
+
+        sword.GetComponent<Animator>().SetTrigger("Attack");
     }
 
     private void OnEnable()
