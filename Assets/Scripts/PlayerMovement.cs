9265761208ec2a4b1d1348370650b4bf3408a82b
Aran Gilman <f.zoniventris@gmail.com>
2024-02-16 12:16:10 -0800

    Add player movement

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,39 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField]
+    private InputActionReference moveAction;
+
+    [SerializeField]
+    private float moveSpeed;
+
+    private Rigidbody2D rb;
+
+    private void HandleMove(InputAction.CallbackContext ctx)
+    {
+        Vector2 val = ctx.ReadValue<Vector2>();
+        rb.velocity = val * moveSpeed;
+        Debug.Log(val);
+    }
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void OnEnable()
+    {
+        moveAction.action.actionMap.Enable();
+        moveAction.action.performed += HandleMove;
+        moveAction.action.canceled += HandleMove;
+    }
+
+    private void OnDisable()
+    {
+        moveAction.action.performed -= HandleMove;
+        moveAction.action.canceled -= HandleMove;
+    }
+}
